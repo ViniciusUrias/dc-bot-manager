6c873ff8f49ab2f6fd04e5c828e75d3a160f255e
ViniciusUrias <viniciusuriasdeabreu@gmail.com>
2025-04-15 10:19:00 -0300

    ajuste url frontend

--- frontend/src/api/services/axios.ts
+++ frontend/src/api/services/axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
-const baseurl = import.meta.env.BACKEND_URL || "localhost:3000/v1";
-console.log(import.meta.env.BACKEND_URL);
+const baseurl = import.meta.env.VITE_BACKEND_URL || "localhost:3000/v1";
+console.log(import.meta.env);
+console.log(import.meta.env.VITE_BACKEND_URL);
 console.log("baseurl", baseurl);
 const axiosInstance = axios.create({
-	baseURL: baseurl,
+	baseURL: baseurl + "/v1",
 	headers: {
 		"Content-Type": "application/json",
 	},
